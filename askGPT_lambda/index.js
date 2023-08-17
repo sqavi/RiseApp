9391cc6f40189bbd1d61fc683d0dcb71ec938094
Shahid Qavi <shahidqavi@gmail.com>
2023-08-17 20:08:12 +0100

    updated reg and verify

--- askGPT_lambda/index.js
+++ askGPT_lambda/index.js
@@ -40,11 +40,43 @@ async function sendMessageToChatPDF(data, config) {
 
 exports.handler = async (event, context) => {
   try {
+    
+    // Check the Authorization datata 
+
+    
+    // Extract the Authorization object from the request body
+    const authorizationData = event.authorization;
+    
+    // Extract values from the Authorization object
+    const user_mobile = authorizationData.user_mobile;
+    const hostname = authorizationData.hostname;
+    const auth_token = authorizationData.auth_token;
+
+            // Retrieve data from DynamoDB
+            const params = {
+              TableName: 'authentication_table',
+              Key: {
+                  'user_mobile': { S: user_mobile },
+                  'hostname': { S: hostname },
+              },
+          };
+  
+          const result = await dynamodb.getItem(params).promise();
+  
+          // Check if the item exists and matches auth_token
+          if (
+              result &&
+              result.Item &&
+              result.Item.auth_token &&
+              result.Item.auth_token.S === auth_token
+          ) 
+
     // Construct the inputData object in the format you need
-    const inputData = {
-      sourceId: sourceId, // Use sourceId (camel case) here
-      messages: event.messages,
-    };
+    {
+        const inputData = {
+        sourceId: sourceId, // Use sourceId (camel case) here
+        messages: event.messages,
+      };
     const config = await generateHeaders();
 
     const response = await sendMessageToChatPDF(inputData, config);
@@ -52,7 +84,7 @@ exports.handler = async (event, context) => {
       statusCode: 200,
       body: JSON.stringify({ response }),
     };
-  
+  } 
   } catch (error) {
     console.error('Error:', error.message);
     return {
